74c5aa7a21ae9518b11c704a7fcc82af0892171c
Noah Lanson <lanson.noah@gmail.com>
2021-07-03 22:34:45 +1000

    equation function added

--- src/main.rs
+++ src/main.rs
@@ -224,6 +224,23 @@ fn draw_curved_line(sp: &Point2, ep: &Point2, direction: X_Y, colour: Srgb, _app
     draw.to_frame(_app, &_frame);
 }
 
+/*
+    Takes in two points, sp and ep, and returns the slope
+    and y intercept for the equation connecting the two.
+*/
+fn find_eq(sp: &Point2, ep: &Point2) -> (f32, f32) {
+    let y1: f32 = sp.y;
+    let y2: f32 = ep.y;
+    let x1: f32 = sp.x;
+    let x2: f32 = ep.x;
+
+    let m: f32 = (y2-y1)/(x2-x1); //m=(y2-y1)/(x2-x1)
+    let b: f32 = (y1) + (m*-x1);  //y-y1=m(x-x1) where b=(m*-x1)+y1
+    
+    //Return slope and y-int as tuple
+    (m, b)
+}
+
 //The larger the distance, the smaller the return value with a minimum of 1.5.
 fn find_div_const(dist: f32) -> f32 {
     3.0
