3b664591de87d7c312eaf9236f657e3009312bf6
Noah Lanson <lanson.noah@gmail.com>
2021-07-06 23:02:06 +1000

    reinstate manual example for reference

--- src/main.rs
+++ src/main.rs
@@ -58,31 +58,8 @@ fn view(_app: &App, _model: &Model, _f: Frame) {
     //Fill in the background as beige.
     draw::fill_background("beige", _app, &_f);
     
-    /*
-        Example that draws stations and lines manually
-
-    //Create example stations
-    let chatswood: Station =    Station::new(pt2(0.0, 300.0), "Chatswood");
-    let st_leonards: Station =  Station::new(pt2(-300.0, 0.0), "St Leonards");
-    let atarmon: Station =      Station::new(pt2(0.0, -300.0), "Atarmon");
-    let north_sydney: Station = Station::new(pt2(300.0, 0.0), "North Sydney");
-
-    //Draw a straight line between example stations
-    sl(&chatswood.coords, &atarmon.coords, "steelblue", _app, &_f);
-    sl(&st_leonards.coords, &north_sydney.coords, "limegreen", _app, &_f);
-
-    //Draw curved lines for example stations
-    cl(&chatswood.coords, &st_leonards.coords, Direction::X, "coral", _app, &_f);
-    cl(&st_leonards.coords, &atarmon.coords, Direction::Y, "coral", _app, &_f);
-    cl(&atarmon.coords, &north_sydney.coords, Direction::X, "coral", _app, &_f);
-    cl(&north_sydney.coords, &chatswood.coords, Direction::Y, "coral", _app, &_f);
-
-    //Draw the stations
-    chatswood.draw(_app, &_f);
-    st_leonards.draw(_app, &_f);
-    atarmon.draw(_app, &_f);
-    north_sydney.draw(_app, &_f);
-    */
+    //Function that draws routes manually
+    draw_manual_example_stations(_app, &_f);
 
     //Example that reads the model and draws segments in each route:
     //For each route
@@ -107,4 +84,28 @@ fn view(_app: &App, _model: &Model, _f: Frame) {
         }
     }
 
+}
+
+fn draw_manual_example_stations(_app: &App, _f: &Frame) {
+    //Create example stations
+    let chatswood: map::station::Station = map::station::Station::new(pt2(0.0, 100.0), "Chatswood");
+    let st_leonards: map::station::Station = map::station::Station::new(pt2(-100.0, 0.0), "St Leonards");
+    let atarmon: map::station::Station = map::station::Station::new(pt2(0.0, -100.0), "Atarmon");
+    let north_sydney: map::station::Station = map::station::Station::new(pt2(100.0, 0.0), "North Sydney");
+
+    //Draw a straight line between example stations
+    sl(&chatswood.coords, &atarmon.coords, "steelblue", _app, _f);
+    sl(&st_leonards.coords, &north_sydney.coords, "limegreen", _app, _f);
+
+    //Draw curved lines for example stations
+    cl(&chatswood.coords, &st_leonards.coords, &Direction::X, "coral", _app, _f);
+    cl(&st_leonards.coords, &atarmon.coords, &Direction::Y, "coral", _app, _f);
+    cl(&atarmon.coords, &north_sydney.coords, &Direction::X, "coral", _app, _f);
+    cl(&north_sydney.coords, &chatswood.coords, &Direction::Y, "coral", _app, _f);
+
+    //Draw the stations
+    chatswood.draw(_app, _f);
+    st_leonards.draw(_app, _f);
+    atarmon.draw(_app, _f);
+    north_sydney.draw(_app, _f);
 }
